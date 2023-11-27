48710482b23d3f01ca9d55fcee2f7e97c08967de
scott chiang <scott01272001@gmail.com>
2023-11-28 07:52:34 +0800

    vue router

---
+++ vue-router-demo/src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import HomeView from "./views/HomeView.vue";
+import AboutView from "./views/AboutView.vue";
+</script>
+
+<template>
+  <div>
+    <HomeView />
+  </div>
+</template>
